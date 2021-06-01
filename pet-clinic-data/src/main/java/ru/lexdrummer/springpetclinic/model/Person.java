7eb7d44484412a1834ebcc636f003ec50cd768f2
Alexandr Zarubin <lex.koenig.drummer@gmail.com>
2021-06-01 20:21:43 +0200

    add Base Entity

--- pet-clinic-data/src/main/java/ru/lexdrummer/springpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/ru/lexdrummer/springpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package ru.lexdrummer.springpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/ru/lexdrummer/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/ru/lexdrummer/springpetclinic/model/Vet.java
@@ -1,4 +1,5 @@
 package ru.lexdrummer.springpetclinic.model;
 
 public class Vet extends Person {
+
 }
