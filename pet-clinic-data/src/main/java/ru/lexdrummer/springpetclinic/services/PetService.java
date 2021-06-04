d1e668dc940af67644b5392faa639ffa09c80273
Alexandr Zarubin <lex.koenig.drummer@gmail.com>
2021-06-04 13:19:40 +0200

    refactor to common crud interface

--- pet-clinic-data/src/main/java/ru/lexdrummer/springpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/ru/lexdrummer/springpetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package ru.lexdrummer.springpetclinic.services;
 
 import ru.lexdrummer.springpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
