f4ebdea02e4d4439477f60379c0353f9d94d1d1d
Alexandr Zarubin <lex.koenig.drummer@gmail.com>
2021-06-04 14:01:07 +0200

    add index page and controller

---
+++ pet-clinic-web/src/main/java/ru/lexdrummer/springpetclinic/conrtollers/IndexController.java
@@ -0,0 +1,15 @@
+package ru.lexdrummer.springpetclinic.conrtollers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index(){
+
+        return "index";
+    }
+}
