7d21b039c7a217ad6ed6577c458e983040e21fb6
Alexandr Zarubin <lex.koenig.drummer@gmail.com>
2021-06-07 13:55:48 +0200

    add BootStrap package and DataLoader class

--- pet-clinic-data/src/main/java/ru/lexdrummer/springpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/ru/lexdrummer/springpetclinic/services/map/PetServiceMap.java
@@ -2,10 +2,11 @@ package ru.lexdrummer.springpetclinic.services.map;
 
 import ru.lexdrummer.springpetclinic.model.Pet;
 import ru.lexdrummer.springpetclinic.services.CrudService;
+import ru.lexdrummer.springpetclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
--- pet-clinic-data/src/main/java/ru/lexdrummer/springpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/ru/lexdrummer/springpetclinic/services/map/VetServiceMap.java
@@ -2,10 +2,11 @@ package ru.lexdrummer.springpetclinic.services.map;
 
 import ru.lexdrummer.springpetclinic.model.Vet;
 import ru.lexdrummer.springpetclinic.services.CrudService;
+import ru.lexdrummer.springpetclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
