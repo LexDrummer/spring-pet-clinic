24d58f955670d51b6479175e8e1e7cc45ae3a265
Alexandr Zarubin <lex.koenig.drummer@gmail.com>
2021-06-11 12:43:32 +0200

    add info properties to Owner

--- pet-clinic-data/src/main/java/ru/lexdrummer/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/ru/lexdrummer/springpetclinic/model/Owner.java
@@ -4,6 +4,35 @@ import java.util.Set;
 
 public class Owner extends Person {
 
+    private String address;
+    private String phone;
+    private String city;
+    private Set<Pet> pets;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
     public Set<Pet> getPets() {
         return pets;
     }
@@ -11,7 +40,5 @@ public class Owner extends Person {
     public void setPets(Set<Pet> pets) {
         this.pets = pets;
     }
-
-    private Set<Pet> pets;
 
 }
