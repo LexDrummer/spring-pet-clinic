321273c57d8aca6dc4314cf06a219d7eafec52d2
Alexandr Zarubin <lex.koenig.drummer@gmail.com>
2021-06-15 20:26:32 +0200

    update Person, Owner, Pet and PetType Entity

--- pet-clinic-data/src/main/java/ru/lexdrummer/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/ru/lexdrummer/springpetclinic/model/Owner.java
@@ -1,13 +1,21 @@
 package ru.lexdrummer.springpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+    @Column(name = "address")
     private String address;
+    @Column(name = "phone")
     private String phone;
+    @Column(name = "city")
     private String city;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
